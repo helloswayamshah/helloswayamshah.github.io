d43e9cf958cbd991a011fbf9dd3b57d190b35f35
swayam shah <helloswayamshah@gmail.com>
2022-10-01 22:47:21 -0700

    navbar color

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,18 +2,18 @@ import Nav from 'react-bootstrap/Nav';
 
 function BasicExample() {
   return (
-    <Nav className="justify-content-end" color= "#64ffda"
+    <Nav className="justify-content-end" 
       activeKey="/home"
       onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
     >
       <Nav.Item>
-        <Nav.Link href="/home">Active</Nav.Link>
+        <Nav.Link href="/home" color='white'>Active</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="link-1">Link</Nav.Link>
+        <Nav.Link eventKey="link-1" color='white'>Link</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
+        <Nav.Link eventKey="link-2" color='white'>Link</Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link eventKey="disabled" disabled>
