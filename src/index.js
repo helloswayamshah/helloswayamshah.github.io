ee229ea34043f83c14beae23a2df8b4d810d0277
swayam shah <helloswayamshah@gmail.com>
2022-10-01 21:57:13 -0700

    Nav Bar added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BasicExample from './Navigation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+
+root.render(
+  <React.StrictMode>
+    <BasicExample />
   </React.StrictMode>
 );
 
